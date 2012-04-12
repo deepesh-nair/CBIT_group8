2022b99dd2d4ff67d5f0f9272a3230a81ac19c4f
Deepesh Nair <dnair6@gatech.edu>
2012-04-12 14:13:30 -0400

    added name sql in homepage

--- Homepage.aspx.cs
+++ Homepage.aspx.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using MySQLHandler;
+using System.Data;
 
 
 namespace CBIT_group8
@@ -10,11 +12,14 @@ namespace CBIT_group8
     public partial class Homepage : System.Web.UI.Page
     {
         public string user;
+        mysqlhandler _mysqlhandler = new mysqlhandler();
 
         protected void Page_Load(object sender, EventArgs e)
         {
             user = Request.QueryString["user"];
-           
+            string sql = "SELECT name FROM `user` WHERE email='" + user + "';";
+            DataTable dt = _mysqlhandler.SelectFromDB(sql);
+            lblName.Text = dt.Rows[0][0].ToString();
         }
     }
 }
